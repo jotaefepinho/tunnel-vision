6ed68c53c1d384198f5940e502cd30b7ab764d59
jotaefepinho <jotaefepinho@gmail.com>
2024-10-13 23:12:32 -0300

    remove personal email

--- tunnelvision/utils.py
+++ tunnelvision/utils.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 def send_alert_email(config, action):
     message = f"Alerta: Hora de {action} {config.asset.symbol} com preço {config.asset.get_B3_quote()}"
 
-    default_sender = 'your@email.com'
+    default_sender = 'youremail@yourserver.com'
     
     send_mail(
         f'{action.capitalize()}: Alerta para {config.asset.symbol}',
